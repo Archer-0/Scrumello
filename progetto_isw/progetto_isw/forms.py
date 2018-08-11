490208fbd84f48281cc5b9b10f4850f5ff2fe4ac
Archer <matt76phone@gmail.com>
2018-08-11 17:25:19 +0200

    Modificato file forms aggiungendo widget

--- progetto_isw/progetto_isw/forms.py
+++ progetto_isw/progetto_isw/forms.py
@@ -2,20 +2,64 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label="Username", max_length=32, required=True)
-    password = forms.CharField(label="Password", max_length=32, required=True, widget=forms.PasswordInput)
+    username = forms.CharField(
+        label="Username",
+        max_length=32,
+        required=True,
+        widget=forms.TextInput(
+            attrs={'class': 'forms_field-input',
+                   'placeholder': 'Username',
+                   'required': 'true',
+                   'autofocus': 'true'}))
+
+    password = forms.CharField(
+        label="Password",
+        max_length=32,
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={'class': 'forms_field-input',
+                   'placeholder': 'Password',
+                   'required': 'true'}))
 
 
 class SignupForm(forms.Form):
-    username = forms.CharField(label="Username", max_length=32, required=True)
-    password = forms.CharField(label="Password", max_length=32, required=True, widget=forms.PasswordInput)
-    password_confirm = forms.CharField(label="Repeat Password", max_length=32, required=True, widget=forms.PasswordInput)
+    username = forms.CharField(
+        label="Username",
+        max_length=32,
+        required=True,
+        widget=forms.TextInput(
+            attrs={'class': 'forms_field-input',
+                   'placeholder': 'Username',
+                   'required': 'true',
+                   'autofocus': 'true'}))
+
+    password = forms.CharField(
+        label="Password",
+        max_length=32,
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={'class': 'forms_field-input',
+                   'placeholder': 'Password',
+                   'required': 'true'}))
+
+    password_confirm = forms.CharField(
+        label="Repeat Password",
+        max_length=32,
+        widget=forms.PasswordInput(
+            attrs={'class': 'forms_field-input',
+                   'placeholder': 'Repeat Password',
+                   'required': 'true'}))
 
 
 class BoardCreationForm(forms.Form):
-    name = forms.CharField(label="Board Name", max_length=512, required=True)
+    board_name = forms.CharField(
+        label="Board Name",
+        max_length=512,
+        required=True)
 
 
 class ColumnCreationForm(forms.Form):
-    name = forms.CharField(label="Column Name", max_length=512, required=True)
-
+    column_name = forms.CharField(
+        label="Column Name",
+        max_length=512,
+        required=True)
