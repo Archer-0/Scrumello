d415108a65826f6b2ac6001760f13691fe9018d5
Assandrino <axxandro@gmail.com>
2018-08-28 17:03:31 +0200

    aggiornato il file test.py

--- progetto_isw/progetto_isw/tests/test_urls.py
+++ progetto_isw/progetto_isw/tests/test_urls.py
@@ -5,14 +5,29 @@ from django.urls import reverse
 
 class UrlTests(TestCase):
 
-    def test_url_dashboard(self):
+    def test_url_login_signup(self):
         """
             test per validare il nome della URL
 
             il test deve passare
         """
 
-        url = reverse('dashboard')  # reverse('test') restituisce '/test/'
-        self.assertEqual(url, '/dashboard/')  # assertEqual verifica se i due parametri sono uguali
+        url = reverse('login_signup')  # reverse('test') restituisce '/test/'
+        self.assertEqual(url, '/')  # assertEqual verifica se i due parametri sono uguali
+
+    def test_url_about(self):
+
+        url = reverse('about')
+        self.assertEqual(url, '/about/')
+
+    def test_url_dashboard(self):
+
+        url = reverse('dashboard')
+        self.assertEqual(url, '/dashboard/')
+
+    def test_url_logout(self):
+
+        url = reverse('logout')
+        self.assertEqual(url, '/logout/')
 
 # comando python per far partire il test: python manage.py test
