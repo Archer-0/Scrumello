c5cf3ea3c287247bda6e769c8f62739910a994c6
Archer <matt76phone@gmail.com>
2018-08-30 23:20:51 +0200

    _ Aggiunto test url burndown

--- progetto_isw/progetto_isw/tests/test_urls.py
+++ progetto_isw/progetto_isw/tests/test_urls.py
@@ -174,6 +174,11 @@ class UrlTestsView(TestCase):   # test sugli URL presenti nel file urls.py e la 
         resolver = resolve('/search_user_card/')
         self.assertEqual(resolver.view_name, 'search_user_card')
 
+    def test_url_view_burndown(self):
+
+        resolver = resolve('/burndown/')
+        self.assertEqual(resolver.view_name, 'burndown')
+
 
 # comando python per far partire i test solo in questo file: python manage.py test --pattern="test_urls.py
 # comando python per far partire tutti i test: python manage.py test
